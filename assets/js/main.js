a40f90242448d5f567845ca71bf901c396417cf2
Abraham Jeevan Roy <abrahamjroy@gmail.com>
2018-07-07 20:24:30 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,27 @@
 		xsmall:	'(max-width: 480px)',
 		xxsmall: '(max-width: 360px)'
 	});
-
+function cycleBackgrounds() {
+    var index = 0;
+ 
+    $imageEls = $('.container .slide'); // Get the images to be cycled.
+ 
+    setInterval(function () {
+        // Get the next index.  If at end, restart to the beginning.
+        index = index + 1 < $imageEls.length ? index + 1 : 0;
+        
+        // Show the next
+        $imageEls.eq(index).addClass('show');
+        
+        // Hide the previous
+        $imageEls.eq(index - 1).removeClass('show');
+    }, 2000);
+};
+ 
+// Document Ready.
+$(function () {
+    cycleBackgrounds();
+});
 	/**
 	 * Applies parallax scrolling to an element's background image.
 	 * @return {jQuery} jQuery object.
